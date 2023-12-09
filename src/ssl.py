1ad9fec9b7c4ce3f84f7154d33ffb994ce9843ff
Karol Stawowski <stawowski.dev@gmail.com>
2023-12-09 20:42:14 +0100

    fix: remove ssl

--- src/ssl.py
+++
@@ -1,9 +0,0 @@
-import os
-import ssl
-
-
-def verify_ssl_certificate():
-    if not os.environ.get("PYTHONHTTPSVERIFY", "") and getattr(
-        ssl, "_create_unverified_context", None
-    ):
-        ssl._create_default_https_context = ssl._create_unverified_context
